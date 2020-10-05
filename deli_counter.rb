96c74db3db95e295384129817157537ca789968d
Marc Steele <marcsteele2014@gmail.com>
2020-10-05 14:16:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def line(line)
+  case line.length
+  when 0
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: 1. #{line[0]} #{[0..line.length]}"
+  end
+end
+
+def take_a_number(line, name)
+  line.push(name)
+  return `Welcome, #{name}. You are number #{line.length} in line.`
+end
