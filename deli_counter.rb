25650e5e5d7cda0ff6d2d2c254013be5670bc1d6
Marc Steele <marcsteele2014@gmail.com>
2020-10-05 14:36:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,8 @@ def line(line)
   when 0
     puts "The line is currently empty."
   else
-    line.each do |customer|
-      puts "The line is currently: #{line.index}. #{line[customer]}"
+    until counter == line.length
+      puts "The line is currently:" << " #{line.index}. #{line[customer]}"
     end
   end
 end
